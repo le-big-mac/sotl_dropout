2145dd28af1c137c88f527e23fd25d32b10627d8
Charlie <charlielondon02@gmail.com>
2021-10-16 15:36:45 +0100

    slurm

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=24:00:00
+
+# set name of job
+#SBATCH --job-name=lr_test
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=charles.london@wolfson.ox.ac.uk
+
+# run the application
+
+module load python3/anaconda
+source activate generalization
+
+python3 experiment.py --dir="$1"
