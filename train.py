ed42777c344178510f53813b65aa0edf45bb6f66
Charlie <charlielondon02@gmail.com>
2021-12-08 21:20:53 -0500

    just record naive_sotl

--- train.py
+++ train.py
@@ -31,43 +31,6 @@ def sample_then_optimize(p, X_train, y_train, X_test, y_test, k=10, weight_decay
     d = len(X_train[0])
 
     prior_std = sqrt(1/(d+1))
-    noise_std = 1
-
-    sotl_list = []
-    mc_sotl_list = []
-
-    for i in range(n-1):
-        for j in range(k):
-            # sample new weights each time
-            model = LinearModel(d, prior_std, p).double()
-            if i > 0:
-                model, _ = train_to_convergence(model, X_train[:i], y_train[:i], weight_decay=weight_decay,
-                                                early_stopping=True)
-
-            model.eval()
-            neg_loss = - (model(X_train[i]) - y_train[i]).item()**2 / (2*noise_std**2)  # - 1/2 * np.log(2*np.pi*noise_variance)
-            sotl_list.append(neg_loss)
-
-            if j == 0:
-                # draw k Monte Carlo dropout samples to approximate expectation of P(D|theta)
-                # draw samples from approximating dropout distribution
-                # put model in train to get loss with Monte Carlo dropout
-                model.train()
-                for e in range(k):
-                    neg_mc_loss = -(model(X_train[i]) - y_train[i]).item()**2 / (2*noise_std**2)  # - 1/2 * np.log(2*np.pi*noise_variance)
-                    mc_sotl_list.append(neg_mc_loss)
-
-        print("SoTL D<{}: {}".format(i, ', '.join(map(str, sotl_list[-k:]))))
-        print("MC SoTL D<{}: {}".format(i, ', '.join(map(str, mc_sotl_list[-k:]))))
-
-    # L^(D) from Bayesian perspective
-    sotl = (1/k)*sum(sotl_list)
-    mc_sotl = (1/k)*sum(mc_sotl_list)
-
-    X_test = torch.tensor(X_test)
-    y_test = torch.tensor(y_test)
-    X_test.to(device)
-    y_test.to(device)
 
     # final model is model trained on all data
     test_losses = []
@@ -75,7 +38,7 @@ def sample_then_optimize(p, X_train, y_train, X_test, y_test, k=10, weight_decay
     for i in range(k):
         final_model = LinearModel(d, prior_std, p).double()
         final_model, n_sotl = train_to_convergence(final_model, X_train, y_train, weight_decay=weight_decay,
-                                                   early_stopping=True)
+                                                   early_stopping=False)
 
         naive_sotls.append(n_sotl)
 
@@ -91,7 +54,7 @@ def sample_then_optimize(p, X_train, y_train, X_test, y_test, k=10, weight_decay
     print("Test Losses: {}".format(', '.join(map(str, test_losses))))
 
     # return naive_sotl for final model trained on all data
-    return sotl, mc_sotl, naive_sotl, test_loss
+    return naive_sotl, test_loss
 
 
 def train_to_convergence(model, X, y, step_size=0.01, num_steps=500, weight_decay=False, early_stopping=False):
